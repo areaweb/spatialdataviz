2cdd1307623513869c31fb7c010b728b7de1927b
rodrigo <rodrigo@areaweb.cl>
2017-10-31 10:09:34 -0300

    deleted more stuf

--- _obs/tmp/joblib/__main__--Users-rodrigo-projects-datavis-__ipython-input__/geocode/func_code.py
+++
@@ -1,4 +0,0 @@
-# first line: 1
-@memory.cache
-def geocode(query):
-    return geocoder.google(query)
--- _obs/tmp/joblib/__main__--Users-rodrigo-projects-datavis-__ipython-input__/get_artistpage/func_code.py
+++
@@ -1,7 +0,0 @@
-# first line: 1
-@memory.cache
-def get_artistpage(query, idx):
-    query = query.replace(' ', '+')
-    url = 'https://www.setlist.fm/search?page={}&query={}'.format(idx, query)
-    browser = RoboBrowser(history=True, parser='html5lib')
-    return browser
